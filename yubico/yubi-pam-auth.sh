401012641e5b788df96f0b2d97a20ee3bc36eb96
Andre Mattie <amattie@introsec.ca>
2016-12-15 01:27:15 -0500

    Initial commit

---
+++ yubico/yubi-pam-auth.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+################################################################################
+#
+#    filename: yubi-pam-auth.sh
+# description: Configure pam auth to need yubikey for sudo
+#      author: hyper3xpl0iter
+#       email: devel@introsec.ca
+#         GPG: 4375 A9A8 A7B0 BCBC 44BD  DB42 4761 C4EF 2A01 1C69
+#     bitcoin: 1LHsfZrES8DksJ41JAXULimLJjUZJf7Qns
+#        date: 12/15/2016
+#
+################################################################################
+
+#### NOTE ####
+# This script does not cover setting up your Yubikey with the yubikey-personalization-tool
+##############
---
+++ yubico/yubikey4-gpg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+################################################################################
+#
+#    filename: yubikey4-gpg.sh
+# description: Generate and upload GPG keys and ID to Yubikey
+#      author: hyper3xpl0iter
+#       email: devel@introsec.ca
+#         GPG: 4375 A9A8 A7B0 BCBC 44BD  DB42 4761 C4EF 2A01 1C69
+#     bitcoin: 1LHsfZrES8DksJ41JAXULimLJjUZJf7Qns
+#        date: 12/15/2016
+#
+################################################################################
